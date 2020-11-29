13aaf5f52961d5b744652a7bcc7d73f6b6d7f327
LiuLou0104 <liu.lou@northeastern.edu>
2020-11-30 03:43:20 +0800

    【Program Structure】create .gitignore and build program structure

---
+++ src/UserInterface/Canvas/SimuCanvasJPanel.java
@@ -0,0 +1,10 @@
+package UserInterface.Canvas;
+
+/**
+ * @Author: LiuLou
+ * @Description:
+ * @Date: Created in 2:39 2020/11/30
+ * @Modified By:
+ */
+public class SimuCanvasJPanel {
+}
---
+++ src/UserInterface/MultipleSimu/MultiSimuJPanel.java
@@ -0,0 +1,10 @@
+package UserInterface.MultipleSimu;
+
+/**
+ * @Author: LiuLou
+ * @Description:
+ * @Date: Created in 2:38 2020/11/30
+ * @Modified By:
+ */
+public class MultiSimuJPanel {
+}
