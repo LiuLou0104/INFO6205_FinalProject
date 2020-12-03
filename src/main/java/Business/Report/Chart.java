2e3b3305d9b25fb79ddac469d695373ca2d9f1a8
tracy626 <tracy_sphere@live.cn>
2020-12-03 13:48:32 +0800

    create plot and attributes of chart

--- src/main/java/Business/Report/Chart.java
+++ src/main/java/Business/Report/Chart.java
@@ -1,5 +1,23 @@
 package Business.Report;
 
+import java.io.File;
+import java.util.Date;
+
 public class Chart {
     //Date、File
+    private Date date;
+    private String fileName;
+    private File file;
+
+    public Chart() {
+        this.date = new Date();
+    }
+
+    public void drawLineChart() {
+
+    }
+
+    public void drawScatterPlot() {
+
+    }
 }
--- src/main/java/Business/Report/ChartDirectory.java
+++ src/main/java/Business/Report/ChartDirectory.java
@@ -1,4 +1,25 @@
 package Business.Report;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChartDirectory {
+    
+    private List<Chart> chartDirectory;
+
+    public ChartDirectory() {
+        this.chartDirectory = new ArrayList<>();
+    }
+
+    public void add(Chart chart) {
+        this.chartDirectory.add(chart);
+    }
+
+    public List<Chart> getChartDirectory() {
+        return this.chartDirectory;
+    }
+
+    public void setChartDirectory(List<Chart> chartDirectory) {
+        this.chartDirectory = chartDirectory;
+    }
 }
